9f5e624ab6ea4fc7052f60e0e656c6dae7a14ecf
Fedorenko Dmitrij <c0va23@gmail.com>
2013-10-19 11:18:36 +0400

    Added missed index for references.

--- db/migrate/20131019045728_create_questions.rb
+++ db/migrate/20131019045728_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
       t.text :text, null: false
-      t.references :question_category
+      t.references :question_category, index: true
 
       t.timestamps
 
--- db/migrate/20131019050843_create_answers.rb
+++ db/migrate/20131019050843_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.string :value, null: false
-      t.references :question
+      t.references :question, index: true
 
       t.timestamps
 
