1f672da40f9a75b3e408264770b02366c9916eed
Konstantin Liferenko <liferenko@gmail.com>
2013-10-19 16:24:01 +0400

    Fix locale auto detection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   private
+
+  before_filter :set_locale
+  helper_method :current_user
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  helper_method :current_user
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
     self.default_url_options[:locale] = I18n.locale
   end
-  before_filter :set_locale
 end
