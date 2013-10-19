1482dfdf376d875422716a33b1f888c80ce9c9f9
Fedorenko Dmitrij <c0va23@gmail.com>
2013-10-19 12:48:55 +0400

    Added method for fetch next question for room.

--- app/models/room.rb
+++ app/models/room.rb
@@ -5,4 +5,20 @@ class Room < ActiveRecord::Base
   has_many :room_questions, dependent: :destroy
   has_many :questions, through: :room_questions
 
+  def last_room_question
+    room_questions.order(:created_at).last
+  end
+
+  def next_room_question
+    if question = self.next_question
+      self.room_questions.create(question: question)
+    end
+  end
+
+protected
+
+  def next_question
+    Question.where.not(id: self.questions).random
+  end
+
 end
