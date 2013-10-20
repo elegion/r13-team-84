42b45b834fa9f94b2d5cbc68f46569c1fbec8221
Konstantin Liferenko <liferenko@gmail.com>
2013-10-20 20:03:01 +0400

    Dont delete guests

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -13,9 +13,7 @@ class Authentication < ActiveRecord::Base
       auth.provider = auth_hash.provider
       auth.uid = auth_hash.uid
       auth.auth = auth_hash
-      if auth.user.present?
-        user.destroy! if user.guest?
-      else
+      unless auth.user
         user.guest = false
         user.name = auth.auth.info["name"]
         user.avatar = auth.auth.info["image"]
