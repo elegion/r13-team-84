a12336af44e4dc77f590848ed6a8134464a4af17
vladimir.lyukov <v.lyukov@gmail.com>
2013-10-20 07:58:34 +0400

    Fixed asset path in avatar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   end
 
   def avatar(user)
-    src = user.avatar || '/assets/default/avatar.png'
+    src = user.avatar || asset_path('default/avatar.png')
     image_tag(src, alt: "", title: user.name, class: 'user-avatar')
   end
 
