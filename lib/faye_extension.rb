fae8c61717464d981f37cce4992f151e7481041d
Fedorenko Dmitrij <c0va23@gmail.com>
2013-10-20 14:25:50 +0400

    Clear unused code from FayeExtension.

--- lib/faye_extension.rb
+++ lib/faye_extension.rb
@@ -1,26 +1,20 @@
 class FayeExtension
 
-  attr_accessor :room_timers
-
-  def initialize
-    @room_timers = Hash.new
-  end
-
   def incoming(message, callback)
+    p message
     callback.call(message)
-    case message["channel"]
-    when "/meta/subscribe"
-      subscribe(message['subscription'])
-    end
+    # case message["channel"]
+    # when "/meta/subscribe"
+    #   subscribe(message['subscription'])
+    # end
   end
 
 private
 
-  def subscribe(subscription)
+  # def subscribe(subscription)
   #   if %r{/rooms/(?<room_id>\d+)} =~ subscription
   #     room_timers[room_id]
   #   end
-    # EventMachine::Timer.new(10) { p 'timer' }
-  end
+  # end
 
 end
